69a6a7ef3ff49feb979d1ce4fc357de4e940b2c4
Henning <henning.holgersen@gmail.com>
2023-11-08 09:21:07 +0100

    text processing

--- tap_sharepointsites/text_stream.py
+++ tap_sharepointsites/text_stream.py
@@ -21,7 +21,7 @@ class TextStream(sharepointsitesStream):
 
     records_jsonpath = "$.value[*]"
     replication_key = "lastModifiedDateTime"
-    primary_keys = ["_sdc_source_file", "_sdc_row_num"]
+    primary_keys = ["_sdc_source_file"]
 
     # schema_filepath = SCHEMAS_DIR / "files.json"
 
