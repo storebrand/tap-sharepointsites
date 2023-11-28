5275d828ef7cf1b7cce28f2f46995a697d397fb2
Henning Holgersen <henning.holgersen@storebrand.no>
2023-11-28 13:09:33 +0100

    WIP

--- tap_sharepointsites/file_handlers/csv_handler.py
+++ tap_sharepointsites/file_handlers/csv_handler.py
@@ -2,7 +2,6 @@
 
 import csv
 import logging
-import re
 
 LOGGER = logging.getLogger(__name__)
 
@@ -15,13 +14,6 @@ class CSVHandler:
         self.textcontent = textcontent
         self.delimiter = delimiter
 
-    @staticmethod
-    def format_key(key):
-        """Format key."""
-        formatted_key = re.sub(r"[^\w\s]", "", key)
-        formatted_key = re.sub(r"\s+", "_", formatted_key)
-        return formatted_key.lower()
-
     def get_dictreader(self):
         """Read CSV file and return csv DictReader object for the file."""
         dr = csv.DictReader(
@@ -30,7 +22,5 @@ class CSVHandler:
             restkey="_sdc_extra",
             delimiter=self.delimiter,
         )
-
-        dr.fieldnames = [self.format_key(key) for key in dr.fieldnames.copy()]
 
         return dr
